bdb050dd8759d13dbbb66222dfc904b1421b08a9
wangjie05 <wangjie05@51Talk.com>
2017-07-14 17:09:36 +0800

    polyfill

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -5,7 +5,7 @@ const rules = require('./rules')
 const commonPlugin = require('./commonPlugin')
 
 module.exports = {
-  entry: PATHS.ENTRY_PATH,
+  entry: ['babel-polyfill', PATHS.ENTRY_PATH],
   output: {
     path: PATHS.DIST_PATH,
     filename: 'js/[name].bundle.js'
