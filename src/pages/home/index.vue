5079fffa82782cbba56196a070f6fc20e206b4be
ccbabi <kxxw28@gmail.com>
2017-07-17 10:35:27 +0800

    fix

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,27 +1,21 @@
-<template lang="pug">
-  div
-    p {{status}}
-    img(src="../../assets/imgs/logo.png", alt="")
-    demo
+<template>
+  <div>{{status}}</div>
 </template>
 
 <script>
-import demo from 'components/demo'
 export default {
   name: 'home',
   data () {
     return {
       status: 'vue is works!'
     }
-  },
-  components: {
-    demo
   }
 }
 </script>
 
-<style lang="stylus">
-p
-  color green
-  display flex
+<style lang="less">
+p {
+  color: green;
+  display: flex;
+}
 </style>
